239199959c4e22c31f7befbd554c4c95e427c3c2
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-13 15:25:52 -0500

    Update SUMASDEREIMANN_AdolfoFloresHernandez3202.m

--- SUMASDEREIMANN_AdolfoFloresHernandez3202.m
+++ SUMASDEREIMANN_AdolfoFloresHernandez3202.m
@@ -1,26 +1,28 @@
 %Titulo : Sumas de reimannn
-% Descripción: Script para representar la funcion 
+% DescripciÃ³n: Script para representar la funcion 
 	% Autor:Adolfo Flores Hernandez 
-	% Fecha: 29/04/2021
-   clc
-   pkg load symbolic
-  clear
- syms x;
- f =  x.^2+2;
-  a = input ('Inicio de intervalo');
-  b = input('Fin de intervalo ');
-  n = 3-0;
-  dx = (b-a)/n;
-  resultado = zeros(1,n);
-  i =1;
-  fs=subs(f,x,(a+i*dx));
-  fr = fs * dx ;
-  resultado = fr;
-  for i=1:n
-    fs=subs(f,x,(a+1+dx));
-    fr=fs*dx;
-    resultado(i+1)=resultado(i)+fr
-   endfor 
-   fprintf('total es:',double(resultado));
-   x=a:0:b;
+	% Fecha: 29/04/20
+clc 
+%variables que sirven para saber los limites en donde se sacara el area
+
+lim_a=1;
+lim_b=4;
+%fun= (x.^2)+2;
+num_inter=200;
+
+%paso de la funcion
+f = inline ("x.^2-1");
+%Calculo de ?i o h
+h= (lim_b-lim_a)/num_inter;
+% m+1 puntos, m intervalos
+x= lim_a:h:lim_b;
+int=0;
+%para cada intervalo
+for i=1:num_inter;
+  %extremos x(i), x(i+1)
+  xm=(x(i)+x(i+1))/2;
+  int=int+f(xm)*h;
+end 
+fprintf ("el area es: ")
+fprintf("%d", int);
   
