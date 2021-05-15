ba99ffc7be49545dd23342c529d39e2a6166fc26
AdolfoFloresHdz <82457793+AdolfoFloresHdz@users.noreply.github.com>
2021-05-15 00:15:52 -0500

    Update SUMASDEREIMANN_AdolfoFloresHernandez3202.m

--- SUMASDEREIMANN_AdolfoFloresHernandez3202.m
+++ SUMASDEREIMANN_AdolfoFloresHernandez3202.m
@@ -3,26 +3,26 @@
 	% Autor:Adolfo Flores Hernandez 
 	% Fecha: 29/04/20
 clc 
+clc 
 %variables que sirven para saber los limites en donde se sacara el area
 
-lim_a=1;
-lim_b=4;
+a=1;
+b=4;
 %fun= (x.^2)+2;
-num_inter=200;
+inter=200;
 
 %paso de la funcion
-f = inline ("x.^2-1");
+f = inline ("x.^2+2");
 %Calculo de ?i o h
-h= (lim_b-lim_a)/num_inter;
+h= (b-a)/inter;
 % m+1 puntos, m intervalos
-x= lim_a:h:lim_b;
+x= a:h:b;
 int=0;
 %para cada intervalo
-for i=1:num_inter;
+for i=1:inter;
   %extremos x(i), x(i+1)
   xm=(x(i)+x(i+1))/2;
   int=int+f(xm)*h;
 end 
 fprintf ("el area es: ")
 fprintf("%d", int);
-  
